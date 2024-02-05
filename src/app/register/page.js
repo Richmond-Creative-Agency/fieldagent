67e8929a74ebfa3bcd6c9a8f33c691846665914d
amarvingomez <110184318+amarvingomez@users.noreply.github.com>
2024-02-04 21:27:52 -0500

    feature: add Header

---
+++ src/app/register/page.js
@@ -0,0 +1,14 @@
+export default function Page() {
+    return(
+        <form>
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <button>Submit</button>
+        </form>
+    )
+}
