3b2e1dd8c211942f1bcf1e8c3b08d5143d10b7f9
ajmarvingomez <ajmarvingomez@gmail.com>
2024-03-09 17:52:12 -0500

    feature: Add Canvass page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,17 @@ import H1 from "./ui/typography/H1";
 import H2 from "./ui/typography/H2";
 import P from "./ui/typography/P";
 import Link from "next/link";
+import Main from "./ui/components/Main";
 export default function Home() {
   return (
     <>
-    <Header />
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <Main>
       <H1>Welcome Field Agent</H1>
       <H2>What is Field Agent?</H2>
       <P>Field Agent is a set of Tools designed for organizers, by organizers.</P>
       <P>Field Agent is a project of <Link href='https://richmondcreative.agency' >Richmond Creative Agency LLC</Link></P>
-    </main>
+    </Main>
+
     </>
 
   );
