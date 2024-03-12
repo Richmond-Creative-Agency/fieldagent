5308a5c6851a475198195efdafe77ff693e02a8b
ajmarvingomez <ajmarvingomez@gmail.com>
2024-03-11 23:01:35 -0400

    feature: make 100% width

--- src/app/ui/components/Main.jsx
+++ src/app/ui/components/Main.jsx
@@ -7,7 +7,7 @@ export default function Main({children}) {
     return (
         <>
         <Header />
-        <div className="flex min-h-screen flex-col items-center justify-center p-24">
+        <div className="flex w-100 min-h-screen flex-col items-center justify-center p-24">
             {children}
         </div>
         <footer>
