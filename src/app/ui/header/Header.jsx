ffbeb6d12857023e75b4dbf9f078626f4f983347
ajmarvingomez <ajmarvingomez@gmail.com>
2024-03-27 13:40:05 -0400

    feature: Add links

--- src/app/ui/header/Header.jsx
+++ src/app/ui/header/Header.jsx
@@ -2,14 +2,25 @@ import Menu from '../components/Menu';
 import NavButton from '../components/NavButton';
 
 export default function Header() {
-	return (
-		<header className=''>
-			<Menu>
-				<NavButton href='/'>Home</NavButton>
-				<NavButton href='/about'>About</NavButton>
-				<NavButton href='/guide/canvass'>Canvass</NavButton>
-				<NavButton href='/donate'>Donate</NavButton>
-			</Menu>
-		</header>
-	);
+  return (
+    <header className="">
+      <Menu>
+        <NavButton to="/" href="/">
+          Home
+        </NavButton>
+        <NavButton to="/about" href="/about">
+          About
+        </NavButton>
+        <NavButton to="/guide/canvass" href="/guide/canvass">
+          Canvass
+        </NavButton>
+        <NavButton to="/tool/canvassr" href="/tool/canvassr">
+          Canvassr
+        </NavButton>
+        <NavButton to="/donate" href="/donate">
+          Donate
+        </NavButton>
+      </Menu>
+    </header>
+  );
 }
