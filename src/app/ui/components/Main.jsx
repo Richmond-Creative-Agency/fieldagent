3492d37adea71195575b954c751a4bc1ab9a24e2
ajmarvingomez <ajmarvingomez@gmail.com>
2024-03-27 12:57:23 -0400

    feature: Add Suspense to Main

--- src/app/ui/components/Main.jsx
+++ src/app/ui/components/Main.jsx
@@ -2,13 +2,17 @@ import Header from '../header/Header';
 import Link from 'next/link';
 import Menu from './Menu';
 import NavButton from './NavButton';
+import { Suspense } from 'react';
 export default function Main({ children }) {
   return (
-    <>
+    <div className="container mx-auto">
       <Header />
-      <div className="flex w-full min-h-screen flex-col items-center justify-center p-24">
-        {children}
-      </div>
+      <Suspense>
+        <div className="flex w-full min-h-screen flex-col items-center justify-center p-24">
+          {children}
+        </div>
+      </Suspense>
+
       <footer>
         <Menu>
           <NavButton href="http://richmondcreative.agency">
@@ -16,6 +20,6 @@ export default function Main({ children }) {
           </NavButton>
         </Menu>
       </footer>
-    </>
+    </div>
   );
 }
