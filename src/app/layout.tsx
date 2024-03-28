83178d76880e2886bbe149d8d3fdf4b0863b1261
ajmarvingomez <ajmarvingomez@gmail.com>
2024-03-28 13:15:11 -0400

    feature: Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Field Agent",
-  description: "Field Agent by Richmond Creative Agency LLC",
+  title: 'Field Agent',
+  description: 'Field Agent by Richmond Creative Agency LLC',
 };
 
 export default function RootLayout({
@@ -19,7 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics/>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
