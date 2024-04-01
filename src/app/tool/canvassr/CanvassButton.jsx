ea4a6b7711f4b06e1c13406e8f2a1e71d7c7289d
ajmarvingomez <ajmarvingomez@gmail.com>
2024-04-01 11:45:12 -0400

    chore: Documenting bug/features

--- src/app/tool/canvassr/CanvassButton.jsx
+++ src/app/tool/canvassr/CanvassButton.jsx
@@ -1,5 +1,6 @@
 export default function CanvassButton({ children, ...props }) {
   const { className = null } = props;
+  // TODO: #3 Need button hover, active, focus state.
   let classes = 'bg-slate-800 p-2';
   if (className) {
     classes = className;
