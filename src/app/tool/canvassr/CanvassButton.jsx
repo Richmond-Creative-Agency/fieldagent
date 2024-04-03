c3a951274fb09fb1f5c40e9a68c9df8d4d954f5c
ajmarvingomez <ajmarvingomez@gmail.com>
2024-04-03 12:57:01 -0400

    feature: Add button classes

--- src/app/tool/canvassr/CanvassButton.jsx
+++ src/app/tool/canvassr/CanvassButton.jsx
@@ -1,7 +1,8 @@
+import { buttonClasses } from '@/app/ui/classes.js';
 export default function CanvassButton({ children, ...props }) {
   const { className = null } = props;
   // TODO: #3 Need button hover, active, focus state.
-  let classes = 'bg-slate-800 p-2';
+  let classes = buttonClasses;
   if (className) {
     classes = className;
   }
---
+++ src/app/ui/classes.js
@@ -0,0 +1,5 @@
+export const buttonClasses = 'bg-green p-2 hover:bg-[#3D822F]';
+
+export const redButtonClasses = 'bg-red p-2 hover:bg-[#DB524D]';
+
+export const inputClasses = 'text-black';
