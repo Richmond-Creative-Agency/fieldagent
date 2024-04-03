b7b3fdff11d58206d2cde5691a23f7e78a43827a
ajmarvingomez <ajmarvingomez@gmail.com>
2024-04-03 11:50:08 -0400

    feature: Add color palette
    Field Agent Palette
    
    #4D8241
    #3C822E
    #218196
    #023436
    #DCE1DE
    #DB716E
    #DB524D

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     screens: {
-      sm:'320px',
-      md:'768px',
-      lg:'1440px',
+      sm: '320px',
+      md: '768px',
+      lg: '1440px',
+    },
+    colors: {
+      current: 'currentColor',
+      green: '#4D8242',
+      lightBlue: '#218196',
+      primary: '#023436',
+      gray: '#DCE1DE',
+      red: '#DB716E',
     },
   },
   plugins: [],
