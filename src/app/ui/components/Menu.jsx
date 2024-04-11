7bd285ec4f8dc4b559df54234d148bdb9e7309c1
ajmarvingomez <ajmarvingomez@gmail.com>
2024-04-10 21:16:42 -0400

    fix: allow menu to flex-wrap

--- src/app/ui/components/Menu.jsx
+++ src/app/ui/components/Menu.jsx
@@ -1,3 +1,3 @@
 export default function Menu({ children }) {
-  return <menu className="py-4 flex align-start">{children}</menu>;
+  return <menu className="py-4 flex align-start flex-wrap">{children}</menu>;
 }
