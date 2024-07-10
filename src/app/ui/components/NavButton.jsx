18ee4a90906e4cf5cfe3923953e16b0379b2390f
ajmarvingomez <ajmarvingomez@gmail.com>
2024-07-09 21:24:49 -0400

    feature: add opacity on hover

--- src/app/ui/components/NavButton.jsx
+++ src/app/ui/components/NavButton.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export default function NavButton({ children, ...props }) {
   return (
     <li>
-      <a className="p-1" {...props}>
+      <a className="p-1 hover:opacity-75" {...props}>
         {children}
       </a>
     </li>
