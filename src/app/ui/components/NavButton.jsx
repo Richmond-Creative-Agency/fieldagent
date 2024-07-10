196850c0ae62f987ee36a87b6fdb363f4225737b
ajmarvingomez <ajmarvingomez@gmail.com>
2024-07-09 21:25:30 -0400

    feature: add opacity on focus of element

--- src/app/ui/components/NavButton.jsx
+++ src/app/ui/components/NavButton.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export default function NavButton({ children, ...props }) {
   return (
     <li>
-      <a className="p-1 hover:opacity-75" {...props}>
+      <a className="p-1 hover:opacity-75 focus:opacity-75" {...props}>
         {children}
       </a>
     </li>
